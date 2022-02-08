092b741e1a94a53652f160faf2a585feb65b3b9e
klerytondev <kleryton.dev@gmail.com>
2022-02-07 21:39:58 -0300

    novo projeto

---
+++ teste/src/teste/Teste01.java
@@ -0,0 +1,16 @@
+package teste;
+
+public class Teste01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("ATestando Git");
+		System.out.println("teste 02");
+		System.out.println("teste 03");
+		System.out.println("teste 04");
+		
+		
+		
+	}
+
+}
